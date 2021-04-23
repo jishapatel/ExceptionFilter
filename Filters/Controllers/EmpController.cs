7e8e4b1cae31cd0ba76dee704aa09174e01eca71
Jisha Patel <53944911+jishapatel@users.noreply.github.com>
2021-04-23 15:13:19 +0530

    Add project files.

---
+++ Filters/Controllers/EmpController.cs
@@ -0,0 +1,37 @@
+﻿using Filters.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Filters.Controllers
+{
+    //[ExceptionFilter]
+    //Controller level Exception
+    public class EmpController : ApiController
+    {
+        int[] numbers = { 22, 43, 12, 6 };
+
+        [ExceptionFilter]
+        /*Get method https://localhost:44302/api/emp?idx=10&divider=2 this will get you an error bcz idx=10 and
+        there is 4 elements in array IndexOutOfRangeException
+        https://localhost:44302/api/emp?idx=2&divider=2  Proper ans
+        https://localhost:44302/api/emp?idx=2&divider=0  DivideByZeroException
+        https://localhost:44302/api/emp?idx=2&divider=0 FormatException*/
+        [HttpGet]
+        //Get method https://localhost:44302/api/emp?idx=0&divider=2
+        public double division(int idx,int divider)
+        {
+            return numbers[idx] / divider;
+        }
+
+        [ExceptionFilter]
+        //Get method https://localhost:44302/api/emp?idx=10 IndexOutOfRangeException
+        public int GetNumber(int idx)
+        {
+            return numbers[idx];
+        }
+    }
+}
---
+++ Filters/Filters/ExceptionFilter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http.Filters;
+
+
+namespace Filters.Filters
+{
+    public class ExceptionFilter : ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext actionExecutedContext)
+        {
+            base.OnException(actionExecutedContext);
+
+            if(actionExecutedContext.Exception is IndexOutOfRangeException)
+            {
+                HttpResponseMessage response = new HttpResponseMessage(System.Net.HttpStatusCode.BadRequest);
+                response.Content = new StringContent("Provide proper index value!!");
+                response.ReasonPhrase = "Index out of Range";
+                actionExecutedContext.Response = response;
+            }
+
+            else if (actionExecutedContext.Exception is DivideByZeroException)
+            {
+                HttpResponseMessage response = new HttpResponseMessage(System.Net.HttpStatusCode.BadRequest);
+                response.Content = new StringContent("Provide proper numbers value!!");
+                response.ReasonPhrase = "Divide By Zero Not Allowed";
+                actionExecutedContext.Response = response;
+            }
+
+            else
+            {
+                HttpResponseMessage response = new HttpResponseMessage(System.Net.HttpStatusCode.BadRequest);
+                response.Content = new StringContent("Error!! please try again!!");
+                actionExecutedContext.Response = response;
+            }
+        } 
+    }
+}
